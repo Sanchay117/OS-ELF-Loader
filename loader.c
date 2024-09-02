9eedf3fb2981b26c4219474bae935bb288fdce4d
Sanchay Singh <sanchay072@gmail.com>
2024-09-02 20:22:18 +0530

    DONE!

--- loader.c
+++ loader.c
@@ -5,6 +5,7 @@
 Elf32_Ehdr *ehdr; // pointer to ELF header struct
 Elf32_Phdr *phdr; // pointer to Programm header struct
 int seg_ptr = 0;
+char* ELF_file_name;
 
 typedef struct{
   void* address;
@@ -50,23 +51,99 @@ unsigned int    p_align;// ignore this for assignment
 
 int fd;
 void* entry_pt_addr;
+
+// read ELF Header
+void read_elf_header(){
+  ehdr = (Elf32_Ehdr*)(malloc(sizeof(Elf32_Ehdr))); // (Elf32_Ehdr*)-> typecasts void*
+
+  if(read(fd,ehdr,sizeof(Elf32_Ehdr))!= sizeof(Elf32_Ehdr)){
+  	printf("Error: Couldn't read ELF header\n");
+  	exit(1);
+  }
+
+  if (ehdr->e_ident[0] != 0x7f || ehdr->e_ident[1] != 'E' || ehdr->e_ident[2] != 'L' || ehdr->e_ident[3] != 'F') {
+    printf("Error: %s is not an ELF file.\n", ELF_file_name);
+    exit(1);
+  }
+}
+
 /*
  * release memory and other cleanups
  */
 void loader_cleanup() {
-  // free(phdr);
+  // free(phdr); -> Already Done!
   free(ehdr);
   for(int i = 0;i<MAX_SEGMENTS;i++){
     if(segments_for_cleanup[i].address!=NULL){
       if(munmap(segments_for_cleanup[i].address, segments_for_cleanup[i].space)==-1){
         printf("Munmap for freeing segment memory failed");
       }else {
-            printf("Successfully unmapped segment at %p\n", segments_for_cleanup[i].address);
-        }
+        printf("Successfully unmapped segment at %p\n", segments_for_cleanup[i].address);
+      }
     }else{
       break;
     }
   }
+}
+
+// reading programm headers!
+void read_programm_header(const int i,const unsigned short int programm_header_size){
+  phdr = (Elf32_Phdr*)(malloc(sizeof(Elf32_Phdr)));
+  lseek(fd,ehdr->e_phoff + i*programm_header_size,SEEK_SET);
+  
+  if (read(fd, phdr, sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)) {
+    printf("Error: Couldn't read program header\n");
+    exit(1);
+  }
+  
+  // printf("Program Header %d:\n", i);
+  // printf("  Type: %u\n", phdr->p_type);
+  // printf("  Offset: %u\n", phdr->p_offset);
+  // printf("  Virtual Address: %u\n", phdr->p_vaddr);
+  // printf("  File Size: %u\n", phdr->p_filesz);
+  // printf("  Memory Size: %u\n", phdr->p_memsz);
+  // printf("  Flags: %u\n", phdr->p_flags);
+  // printf("  Alignment: %u\n", phdr->p_align);
+
+  // printf("----------------------\n");
+}
+
+// map segment
+void map_segments(const unsigned short int programm_header_size){
+  void* VIRTUAL_MEMORY = mmap(NULL,phdr->p_memsz,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_ANONYMOUS|MAP_PRIVATE,0,0);
+
+  if(VIRTUAL_MEMORY==MAP_FAILED){
+    printf("Error Mapping memory\n");
+    exit(1);
+  }
+
+  lseek(fd,phdr->p_offset,SEEK_SET);
+
+  // Reading data
+  if (read(fd, VIRTUAL_MEMORY, phdr->p_filesz) != phdr->p_filesz) {
+    printf("Error reading segment data\n");
+    exit(1);
+  }
+
+  // Zero out the rest of the memory if the segment is larger than the file size
+  if (phdr->p_filesz < phdr->p_memsz) {
+    memset((char*)VIRTUAL_MEMORY + phdr->p_filesz, 0, phdr->p_memsz - phdr->p_filesz);
+  }
+
+  printf("PT_LOAD Segment loaded at address %p\n", VIRTUAL_MEMORY);
+
+  if (ehdr->e_entry >= phdr->p_vaddr && ehdr->e_entry < phdr->p_vaddr + phdr->p_memsz) {
+    size_t offset_inside_segment = ehdr->e_entry - phdr->p_vaddr;
+    entry_pt_addr = (char*) VIRTUAL_MEMORY + offset_inside_segment;
+    printf("Entry point located at address %p\n", entry_pt_addr);
+  }
+
+  // free(VIRTUAL_MEMORY);
+  // munmap(VIRTUAL_MEMORY,phdr->p_memsz);
+
+  segments_for_cleanup[seg_ptr].address = VIRTUAL_MEMORY;
+  segments_for_cleanup[seg_ptr].space = phdr->p_memsz;
+  seg_ptr++;
 }
 
 /*
@@ -85,8 +162,8 @@ void load_and_run_elf(char** elf_file) {
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
 
-  char* file_name = elf_file[1];
-  // printf("%s %d\n",file_name,fd);
+  char* ELF_file_name = elf_file[1];
+  // printf("%s %d\n",ELF_file_name,fd);
 
   // Now if we have come here clearly we have our Ehdr and fd as well
 
@@ -96,65 +173,15 @@ void load_and_run_elf(char** elf_file) {
   
   for(int i = 0;i<ehdr->e_phnum;i++){
 
-    phdr = (Elf32_Phdr*)(malloc(sizeof(Elf32_Phdr)));
-    lseek(fd,ehdr->e_phoff + i*programm_header_size,SEEK_SET);
-    
-    if (read(fd, phdr, sizeof(Elf32_Phdr)) != sizeof(Elf32_Phdr)) {
-      printf("Error: Couldn't read program header\n");
-      exit(1);
-    }
-    
-    // printf("Program Header %d:\n", i);
-    // printf("  Type: %u\n", phdr->p_type);
-    // printf("  Offset: %u\n", phdr->p_offset);
-    // printf("  Virtual Address: %u\n", phdr->p_vaddr);
-    // printf("  File Size: %u\n", phdr->p_filesz);
-    // printf("  Memory Size: %u\n", phdr->p_memsz);
-    // printf("  Flags: %u\n", phdr->p_flags);
-    // printf("  Alignment: %u\n", phdr->p_align);
-
-    // printf("----------------------\n");
+    read_programm_header(i,programm_header_size);
 
     // now loading PT_LOAD
 
     if(phdr->p_type==PT_LOAD){
-      void* VIRTUAL_MEMORY = mmap(NULL,phdr->p_memsz,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_ANONYMOUS|MAP_PRIVATE,0,0);
-
-      if(VIRTUAL_MEMORY==MAP_FAILED){
-        printf("Error Mapping memory\n");
-        exit(1);
-      }
-
-      lseek(fd,phdr->p_offset,SEEK_SET);
-
-      // Reading data
-      if (read(fd, VIRTUAL_MEMORY, phdr->p_filesz) != phdr->p_filesz) {
-        printf("Error reading segment data\n");
-        exit(1);
-      }
-
-      // Zero out the rest of the memory if the segment is larger than the file size
-      if (phdr->p_filesz < phdr->p_memsz) {
-        memset((char*)VIRTUAL_MEMORY + phdr->p_filesz, 0, phdr->p_memsz - phdr->p_filesz);
-      }
-
-      printf("PT_LOAD Segment loaded at address %p\n", VIRTUAL_MEMORY);
-
-       if (ehdr->e_entry >= phdr->p_vaddr && ehdr->e_entry < phdr->p_vaddr + phdr->p_memsz) {
-          size_t offset_inside_segment = ehdr->e_entry - phdr->p_vaddr;
-          entry_pt_addr = (char*) VIRTUAL_MEMORY + offset_inside_segment;
-          printf("Entry point located at address %p\n", entry_pt_addr);
-       }
-
-      // free(VIRTUAL_MEMORY);
-      // munmap(VIRTUAL_MEMORY,phdr->p_memsz);
-
-      segments_for_cleanup[seg_ptr].address = VIRTUAL_MEMORY;
-      segments_for_cleanup[seg_ptr].space = phdr->p_memsz;
-      seg_ptr++;
+      map_segments(programm_header_size);
     }
     
-    // not unloading this in loader_cleanup as pointer referencing a new address for every programm header
+    // not freeing this in loader_cleanup as pointer referencing a new address for every programm header
     free(phdr);
   }
 
@@ -186,45 +213,33 @@ int main(int argc, char** argv)
     printf("Argument %d: %s\n", i, argv[i]);
   }*/
 
-  char* file_name;
-
   if(argc != 2) {
     printf("Usage: %s <ELF Executable> \nYou need to provide ELF File as an argument\n",argv[0]);
     exit(1);
   }else{
-    file_name = argv[1];
+    ELF_file_name = argv[1];
   }
 
-  // printf("%s\n",file_name);
+  // printf("%s\n",ELF_file_name);
 
   // 1. carry out necessary checks on the input ELF file
 
   // Checking if file exists
-  if (access(file_name, F_OK) != 0) {
-    printf("Error: File %s does not exist.\n", file_name);
+  if (access(ELF_file_name, F_OK) != 0) {
+    printf("Error: File %s does not exist.\n", ELF_file_name);
     exit(1);
   }
 
 	// Checking if we have access to read said file
-  fd = open(file_name, O_RDONLY);
+  fd = open(ELF_file_name, O_RDONLY);
   if (fd < 0) {
  	printf("Error opening file\n");
  	exit(1);
   }
 
-  // Checking if the said file is an ELF
+  // Checking if the said file is an ELF and reading ELF header
 
-  ehdr = (Elf32_Ehdr*)(malloc(sizeof(Elf32_Ehdr))); // (Elf32_Ehdr*)-> typecasts void*
-
-  if(read(fd,ehdr,sizeof(Elf32_Ehdr))!= sizeof(Elf32_Ehdr)){
-  	printf("Error: Couldn't read ELF header\n");
-  	exit(1);
-  }
-
-  if (ehdr->e_ident[0] != 0x7f || ehdr->e_ident[1] != 'E' || ehdr->e_ident[2] != 'L' || ehdr->e_ident[3] != 'F') {
-    printf("Error: %s is not an ELF file.\n", file_name);
-    exit(1);
-  }
+  read_elf_header();  
 
   // 2. passing it to the loader for carrying out the loading/execution
   // Now if we come so far, we are sure that we have an ELF!
